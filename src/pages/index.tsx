36f0d7feedfe567b36f1112cdfc0e3fae4c08c14
hrncirik <hrncacz@gmail.com>
2022-08-16 07:26:44 +0200

    test změny 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,19 @@ import type { NextPage } from 'next';
 import styles from '../styles/index.module.scss';
 
 const Home = () => {
-  return <h1 className={styles.testh1}>AHOJ Martine</h1>;
+  return (
+    <>
+      <h1 className={styles.testh1}>AHOJ Martine</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsam
+        sunt aliquid voluptatem quibusdam perferendis aspernatur, sapiente
+        itaque? Unde laborum quis maxime dicta eum numquam laboriosam cum
+        deleniti sit ex ea harum minima, ducimus ratione. Inventore sequi, velit
+        quas alias, magnam officiis vel atque dignissimos perspiciatis corporis
+        itaque enim dolores.
+      </p>
+    </>
+  );
 };
 
 export default Home;
